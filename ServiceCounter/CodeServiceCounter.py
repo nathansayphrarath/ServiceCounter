c7bc002b9e4ab87b424c8825c4a6f05b724f2608
nathansayphrarath <nathansayphrarath@gmail.com>
2025-03-12 09:27:55 -0400

    Update CodeServiceCounter.py

--- ServiceCounter/CodeServiceCounter.py
+++ ServiceCounter/CodeServiceCounter.py
@@ -1,1 +1,27 @@
+# Project: Service Counter
+# Author: Nathan Sayphrarath
+# Email: nsayphrarath2709522@woonsocketschools.com
+# Date: 3-12-2025
+# Description: I'm making an app for the WACTC Garage
+
+
+print("Welcome to the WACTC Garage!") # Welcome
+print()
+
+name = input ("What is your name?:") # Asking name
+print()
+
+print(f"Hello {name}!")
+print()
+
+make_Model = input("What is the make and model of your vehicle?: ") # Asking the make and model
+
+mileage = input("Okay so what is the mileage on your car?:") # Asking for mileage
+print()
+
+service = input("""
+Here is a list of our available services:
+
+""")
+
 
